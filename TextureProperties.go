92576a1eb10f0e4671b1d7eb2d6268dea6e5ccac
Christian Haas <christian.haas@sevensuns.at>
2015-11-09 16:23:47 +0100

    texture documentation

--- TextureProperties.go
+++ TextureProperties.go
@@ -1,7 +1,10 @@
 package model
 
+// TextureProperties contain all the behavioural settings of a texture.
 type TextureProperties struct {
-	Name       [LanguageCount]*string `json:"name"`
+	// Name is the (language specific) name of the texture.
+	Name [LanguageCount]*string `json:"name"`
+	// CantBeUsed is a (language specific) text for usage failures.
 	CantBeUsed [LanguageCount]*string `json:"cantBeUsed"`
 
 	//Climbable bool `json:"climbable"`
---
+++ TextureSize.go
@@ -0,0 +1,11 @@
+package model
+
+// TextureSize is an enumeration of allowed texture sizes.
+type TextureSize string
+
+const (
+	TextureIcon   TextureSize = "icon"
+	TextureSmall              = "small"
+	TextureMedium             = "medium"
+	TextureLarge              = "large"
+)

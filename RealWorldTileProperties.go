2f557bc36fcc0913fa0a0fc2f26ba659ffd607e4
Christian Haas <christian.haas@sevensuns.at>
2015-11-16 19:11:06 +0100

    added Level/Map properties

---
+++ RealWorldTileProperties.go
@@ -0,0 +1,10 @@
+package model
+
+type RealWorldTileProperties struct {
+	FloorTexture   int `json:"floorTexture"`
+	CeilingTexture int `json:"ceilingTexture"`
+	WallTexture    int `json:"wallTexture"`
+
+	FloorTextureRotations   int `json:"floorTextureRotations"`
+	CeilingTextureRotations int `json:"ceilingTextureRotations"`
+}
---
+++ TileProperties.go
@@ -0,0 +1,12 @@
+package model
+
+type TileProperties struct {
+	Type TileType `json:"type"`
+
+	FloorHeight   HeightUnit `json:"floorHeight"`
+	CeilingHeight HeightUnit `json:"ceilingHeight"`
+	SlopeHeight   HeightUnit `json:"slopeHeight"`
+
+	RealWorld *RealWorldTileProperties `json:"realWorld"`
+	//Cyberspace *CyberspaceTileProperties
+}

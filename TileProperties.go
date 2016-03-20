d3bd23001f9be1488d4e2d04c4e1e9f35bc0421c
Christian Haas <christian.haas@sevensuns.at>
2016-03-20 21:28:09 +0100

    added calculated wall heights to tile properties

--- TileProperties.go
+++ TileProperties.go
@@ -7,6 +7,8 @@ type TileProperties struct {
 	CeilingHeight *HeightUnit `json:"ceilingHeight"`
 	SlopeHeight   *HeightUnit `json:"slopeHeight"`
 
+	CalculatedWallHeights *CalculatedWallHeights `json:"calculatedWallHeights"`
+
 	RealWorld *RealWorldTileProperties `json:"realWorld"`
 	//Cyberspace *CyberspaceTileProperties
 }

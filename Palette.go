192adc9f8a4fd05947ecef9d5ba773c6d975c1fa
Christian Haas <christian.haas@sevensuns.at>
2017-06-05 21:07:34 +0200

    removed all remnants of the attempted REST interface

--- Palette.go
+++ Palette.go
@@ -2,15 +2,13 @@ package model
 
 // Color describes a single color value. The channels have a range of 0..255 .
 type Color struct {
-	Red   int `json:"r"`
-	Green int `json:"g"`
-	Blue  int `json:"b"`
+	Red   int
+	Green int
+	Blue  int
 }
 
 // Palette is an identifiable list of colors.
 type Palette struct {
-	Identifiable
-
 	// Colors contains the color values of the palette.
-	Colors [256]Color `json:"colors"`
+	Colors [256]Color
 }

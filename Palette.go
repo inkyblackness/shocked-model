cc5f2e53c8be9ec6c7977c0d20e1a8fb4a266a58
Christian Haas <christian.haas@sevensuns.at>
2017-06-15 14:14:43 +0200

    made Color implement image.Color interface

--- Palette.go
+++ Palette.go
@@ -7,6 +7,16 @@ type Color struct {
 	Blue  int
 }
 
+// RGBA implements the image/color interface.
+func (clr Color) RGBA() (r, g, b, a uint32) {
+	upscale := func(value int) uint32 {
+		b := uint32(value & 0xFF)
+		return (b << 8) | b
+	}
+
+	return upscale(clr.Red), upscale(clr.Green), upscale(clr.Blue), 0xFFFF
+}
+
 // Palette is an identifiable list of colors.
 type Palette struct {
 	// Colors contains the color values of the palette.

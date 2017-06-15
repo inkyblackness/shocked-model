cd54e89eaeae9de30dad4b2981443d26041f5cf0
Christian Haas <christian.haas@sevensuns.at>
2017-06-15 16:08:12 +0200

    Resource keys can be localized now

--- ResourceKey.go
+++ ResourceKey.go
@@ -6,28 +6,42 @@ import (
 
 // ResourceKey is the reference of a specific game resource.
 type ResourceKey struct {
-	Type  ResourceType
-	Index uint16
+	Type     ResourceType
+	Language ResourceLanguage
+	Index    uint16
 }
 
 // MakeResourceKey returns a combined resource identifier.
 func MakeResourceKey(resourceType ResourceType, index uint16) ResourceKey {
-	return ResourceKey{resourceType, index}
+	return ResourceKey{Type: resourceType, Language: ResourceLanguageUnspecific, Index: index}
+}
+
+// MakeLocalizedResourceKey returns a combined resource identifier with specified language.
+func MakeLocalizedResourceKey(resourceType ResourceType, language ResourceLanguage, index uint16) ResourceKey {
+	return ResourceKey{Type: resourceType, Language: language, Index: index}
 }
 
 // ResourceKeyFromInt returns a resource identifier wrapping the provided integer.
 func ResourceKeyFromInt(value int) ResourceKey {
 	return ResourceKey{
-		Type:  ResourceType(uint16((value >> 16) & 0xFFFF)),
-		Index: uint16(value & 0xFFFF)}
+		Type:     ResourceType(uint16((value >> 16) & 0xFFFF)),
+		Language: ResourceLanguage((value >> 13) & 0x3),
+		Index:    uint16(value & 0x1FFF)}
+}
+
+// HasValidLanguage returns true if the Language field is within range [1..3].
+func (id ResourceKey) HasValidLanguage() bool {
+	return (int(id.Language) >= 1) && (int(id.Language) <= 3)
 }
 
 // ToInt returns a single integer representation of the ID.
 func (id ResourceKey) ToInt() int {
-	return (int(id.Type) << 16) | int(id.Index)
+	return (int(id.Type) << 16) | (int(id.Language) << 13) | int(id.Index)
 }
 
 // String implements the Stringer interface.
 func (id ResourceKey) String() string {
-	return fmt.Sprintf("0x%04X:%03d", uint16(id.Type), id.Index)
+	languages := []string{"*", "STD", "FRN", "GER"}
+
+	return fmt.Sprintf("0x%04X:%03d[%v]", uint16(id.Type), id.Index, languages[id.Language])
 }
